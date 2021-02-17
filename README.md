2294434d1b85b313950fdc31e3b4a654ac92f4ba
Virus <Virus@DESKTOP-116NMUM>
2021-02-17 19:51:45 +0300

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1>Init repository</h1>
---
+++ modules/clicksco/clicksco.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\clicksco;
+
+/**
+ * clicksco module definition class
+ */
+class clicksco extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\clicksco\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
